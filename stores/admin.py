74f425eebde79a673db17ddd9eda6ae00f385e14
AlaaNaser95 <alaanaser95.95@gmail.com>
2018-10-18 12:07:29 +0300

    task submission

--- stores/admin.py
+++ stores/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Store
 
 # Register your models here.
-admin.site.register(Store)
+
+class StoreAdmin(admin.ModelAdmin):
+	list_display = ('name','id', 'description')
+	list_filter = ['added']
+	search_fields = ['name','description']
+	list_editable=['description']
+admin.site.register(Store, StoreAdmin)
